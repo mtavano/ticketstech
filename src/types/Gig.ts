b1c07c946db638842ddf2e5f15a4a9580265bb69
mtavano <chelotavano.udp@gmail.com>
2019-04-12 14:35:11 -0400

    Add Host and Gig types

---
+++ src/types/Gig.ts
@@ -0,0 +1,13 @@
+type Gig = {
+  id: string;
+  place: string;
+  eventDate: string;
+  fbLink: string;
+  description: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date;
+};
+
+export default Gig;
---
+++ src/types/Host.ts
@@ -0,0 +1,10 @@
+type Host = {
+  id: string;
+  name: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date;
+};
+
+export default Host;
